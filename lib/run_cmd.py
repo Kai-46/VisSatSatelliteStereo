ec45a546a04fe6bca5c30ab7e3c33c0cf5a0c8e9
Kai-46 <kz298@cornell.edu>
2018-12-14 19:11:03 -0500

    interact with long-running subprocesses

--- lib/run_cmd.py
+++ lib/run_cmd.py
@@ -8,11 +8,19 @@ def run_cmd(cmd):
     logging.info('Running subprocess: {}'.format(cmd))
 
     try:
-        p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        output, _ = p.communicate()
-        output = output.decode('ascii')
-        logging.info(output)
+        process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # output, _ = process.communicate()
+        # output = output.decode('ascii')
+        # logging.info(output)
         #print('output: {}'.format(output))
+
+        # interacting with long-running subprocess
+        while True:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                logging.info(output)
     except (OSError, subprocess.CalledProcessError) as exception:
         logging.error('Exception occured: {}'.format(exception))
         logging.error('Subprocess failed')

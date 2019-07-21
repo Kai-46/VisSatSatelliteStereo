7f26fc7a275789268aaa324aad2150160c466f47
Kai Zhang <kz298@cornell.edu>
2019-07-21 14:50:46 +0800

    Update coordinate_system.py

--- coordinate_system.py
+++ coordinate_system.py
@@ -8,21 +8,18 @@ from lib.latlonalt_enu_converter import latlonalt_to_enu, enu_to_latlonalt
 # local: enu
 
 def local_to_global(work_dir, xx, yy, zz):
-    # with open(os.path.join(work_dir, 'aoi.json')) as fp:
-    #     bbx = json.load(fp)
-    # # lat0 = bbx['lat_min']
-    # # lon0 = bbx['lon_min']
-    # # alt0 = bbx['alt_min']
-    #
-    # lat0 = (bbx['lat_min'] + bbx['lat_max']) / 2.0
-    # lon0 = (bbx['lon_min'] + bbx['lon_max']) / 2.0
-    # alt0 = bbx['alt_min']
+    with open(os.path.join(work_dir, 'aoi.json')) as fp:
+        bbx = json.load(fp)
+    
+    lat0 = (bbx['lat_min'] + bbx['lat_max']) / 2.0
+    lon0 = (bbx['lon_min'] + bbx['lon_max']) / 2.0
+    alt0 = bbx['alt_min']
 
-    with open(os.path.join(work_dir, 'geo_grid.json')) as fp:
-        geo_grid = json.load(fp)
-    lat0 = geo_grid['observer_lat']
-    lon0 = geo_grid['observer_lon']
-    alt0 = geo_grid['observer_alt']
+#     with open(os.path.join(work_dir, 'geo_grid.json')) as fp:
+#         geo_grid = json.load(fp)
+#     lat0 = geo_grid['observer_lat']
+#     lon0 = geo_grid['observer_lon']
+#     alt0 = geo_grid['observer_alt']
 
     xx, yy, zz = enu_to_latlonalt(xx, yy, zz, lat0, lon0, alt0)
 
@@ -30,21 +27,18 @@ def local_to_global(work_dir, xx, yy, zz):
 
 
 def global_to_local(work_dir, xx, yy, zz):
-    # with open(os.path.join(work_dir, 'aoi.json')) as fp:
-    #     bbx = json.load(fp)
-    # # lat0 = bbx['lat_min']
-    # # lon0 = bbx['lon_min']
-    # # alt0 = bbx['alt_min']
-    #
-    # lat0 = (bbx['lat_min'] + bbx['lat_max']) / 2.0
-    # lon0 = (bbx['lon_min'] + bbx['lon_max']) / 2.0
-    # alt0 = bbx['alt_min']
+    with open(os.path.join(work_dir, 'aoi.json')) as fp:
+        bbx = json.load(fp)
 
-    with open(os.path.join(work_dir, 'geo_grid.json')) as fp:
-        geo_grid = json.load(fp)
-    lat0 = geo_grid['observer_lat']
-    lon0 = geo_grid['observer_lon']
-    alt0 = geo_grid['observer_alt']
+    lat0 = (bbx['lat_min'] + bbx['lat_max']) / 2.0
+    lon0 = (bbx['lon_min'] + bbx['lon_max']) / 2.0
+    alt0 = bbx['alt_min']
+
+#     with open(os.path.join(work_dir, 'geo_grid.json')) as fp:
+#         geo_grid = json.load(fp)
+#     lat0 = geo_grid['observer_lat']
+#     lon0 = geo_grid['observer_lon']
+#     alt0 = geo_grid['observer_alt']
 
     xx, yy, zz = latlonalt_to_enu(xx, yy, zz, lat0, lon0, alt0)
 

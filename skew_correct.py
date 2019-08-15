d5efb40bd4719c51bafc5501528f1efc08c20bbe
Walt Dixon <wdixon@users.noreply.github.com>
2019-08-15 15:33:21 -0400

    skew correction was taking 1/4 of the pipline time. Added parallelization

--- skew_correct.py
+++ skew_correct.py
@@ -1,5 +1,3 @@
-import multiprocessing
-
 import numpy as np
 import json
 import logging
@@ -8,41 +6,6 @@ import imageio
 from lib.warp_affine import warp_affine
 from colmap.extract_sfm import extract_all_tracks
 import shutil
-
-
-def skew_correct_worker(work_dir, params, img_name):
-    perspective_img_dir = os.path.join(work_dir, 'colmap/sfm_perspective/images')
-    out_dir = os.path.join(work_dir, 'colmap/skew_correct')
-    pinhole_img_dir = os.path.join(out_dir, 'images')
-
-    fx = params[2]
-    fy = params[3]
-    cx = params[4]
-    cy = params[5]
-    s = params[6]
-    qvec = params[7:11]
-    tvec = params[11:14]
-
-    # compute homography and update s, cx
-    norm_skew = s / fy
-    cx = cx - s * cy / fy
-    # s = 0.
-
-    # warp image
-    affine_matrix = np.array([[1., -norm_skew, 0.],
-                              [0., 1., 0.]])
-    img_src = imageio.imread(os.path.join(perspective_img_dir, img_name))
-    img_dst, off_set, affine_matrix = warp_affine(img_src, affine_matrix)
-    imageio.imwrite(os.path.join(pinhole_img_dir, img_name), img_dst)
-
-    new_h, new_w = img_dst.shape[:2]
-    # add off_set to camera parameters
-    cx += off_set[0]
-    cy += off_set[1]
-
-    return norm_skew, affine_matrix, [new_w, new_h, fx, fy, cx, cy,
-            qvec[0], qvec[1], qvec[2], qvec[3],
-            tvec[0], tvec[1], tvec[2]]
 
 
 def skew_correct(work_dir):
@@ -64,34 +27,50 @@ def skew_correct(work_dir):
     pinhole_dict = {}
     affine_warping_dict = {}
     info_txt = 'img_name, skew (s/fy)\n'
-
-    results = []
-    arguments = []
-    perspective_images = os.listdir(perspective_img_dir)
-
-    pool_size = min(multiprocessing.cpu_count(), len(perspective_images))
-    pool = multiprocessing.Pool(pool_size)
-
-    for img_name in sorted(perspective_images):
+    for img_name in sorted(os.listdir(perspective_img_dir)):
         # w, h, fx, fy, cx, cy, s, qvec, t
         params = perspective_dict[img_name]
         w = params[0]
         h = params[1]
-        arguments.append((img_name, w, h))
-        results.append(pool.apply_async(skew_correct_worker, (work_dir, params, img_name)))
+        fx = params[2]
+        fy = params[3]
+        cx = params[4]
+        cy = params[5]
+        s = params[6]
+        qvec = params[7:11]
+        tvec = params[11:14]
 
-    for i, r in enumerate(results):
-        img_name, w, h = arguments[i]
-        norm_skew, affine_matrix, pinhole = r.get()
+        # compute homography and update s, cx
+        norm_skew = s / fy
+        cx = cx - s * cy / fy
+        # s = 0.
+
+        # warp image
+        affine_matrix = np.array([[1., -norm_skew, 0.],
+                                  [0., 1., 0.]])
+        img_src = imageio.imread(os.path.join(perspective_img_dir, img_name))
+        img_dst, off_set, affine_matrix = warp_affine(img_src, affine_matrix)
+        imageio.imwrite(os.path.join(pinhole_img_dir, img_name), img_dst)
+
         affine_warping_dict[img_name] = affine_matrix
-        logging.info(
-            'removed normalized skew: {} in image: {}, original size: {}, {}, new image size: {}, {}'.format(
-                norm_skew, img_name, w, h, pinhole[0], pinhole[1]))
-        info_txt += '{}, {}\n'.format(img_name, norm_skew)
-        pinhole_dict[img_name] = pinhole
 
-    pool.close()
-    pool.join()
+        new_h, new_w = img_dst.shape[:2]
+        # add off_set to camera parameters
+        cx += off_set[0]
+        cy += off_set[1]
+
+        logging.info(
+            'removed normalized skew: {} in image: {}, original size: {}, {}, new image size: {}, {}'.format(norm_skew,
+                                                                                                             img_name,
+                                                                                                             w, h,
+                                                                                                             new_w,
+                                                                                                             new_h))
+
+        info_txt += '{}, {}\n'.format(img_name, norm_skew)
+
+        pinhole_dict[img_name] = [new_w, new_h, fx, fy, cx, cy,
+                                  qvec[0], qvec[1], qvec[2], qvec[3],
+                                  tvec[0], tvec[1], tvec[2]]
 
     with open(pinhole_file, 'w') as fp:
         json.dump(pinhole_dict, fp, indent=2)

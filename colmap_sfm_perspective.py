58cd501112a3cdcae4493889c6f44c76e58d53b9
Kai Zhang <kz298@cornell.edu>
2019-08-30 08:49:03 +0800

    skip an unnecessary triangulation step in order to save time

--- colmap_sfm_perspective.py
+++ colmap_sfm_perspective.py
@@ -8,11 +8,17 @@ import shutil
 
 
 def make_subdirs(sfm_dir):
+#     subdirs = [
+#                 sfm_dir,
+#                 os.path.join(sfm_dir, 'init_triangulate'),
+#                 os.path.join(sfm_dir, 'init_triangulate_ba'),
+#                 os.path.join(sfm_dir, 'init_ba_triangulate')
+#     ]
+
     subdirs = [
                 sfm_dir,
                 os.path.join(sfm_dir, 'init_triangulate'),
-                os.path.join(sfm_dir, 'init_triangulate_ba'),
-                os.path.join(sfm_dir, 'init_ba_triangulate')
+                os.path.join(sfm_dir, 'init_triangulate_ba')
     ]
 
     for item in subdirs:
@@ -49,12 +55,12 @@ def run_sfm(work_dir, sfm_dir, init_camera_file, weight):
     camera_dict = extract_camera_dict(out_dir)
     with open(os.path.join(sfm_dir, 'init_ba_camera_dict.json'), 'w') as fp:
         json.dump(camera_dict, fp, indent=2, sort_keys=True)
-    init_ba_template = os.path.join(sfm_dir, 'init_ba_template.json')
-    write_template_perspective(camera_dict, init_ba_template)
-
-    out_dir = os.path.join(sfm_dir, 'init_ba_triangulate')
-    # colmap_sfm_commands.run_point_triangulation(img_dir, db_file, out_dir, init_ba_template, 1.5, 2, 2)
-    colmap_sfm_commands.run_point_triangulation(img_dir, db_file, out_dir, init_ba_template, 4.0, 4.0, 4.0)
+    
+#     init_ba_template = os.path.join(sfm_dir, 'init_ba_template.json')
+#     write_template_perspective(camera_dict, init_ba_template)
+#     out_dir = os.path.join(sfm_dir, 'init_ba_triangulate')
+#     # colmap_sfm_commands.run_point_triangulation(img_dir, db_file, out_dir, init_ba_template, 1.5, 2, 2)
+#     colmap_sfm_commands.run_point_triangulation(img_dir, db_file, out_dir, init_ba_template, 4.0, 4.0, 4.0)
     
     # for later uses: check how big the image-space translations are
     with open(os.path.join(sfm_dir, 'init_camera_dict.json')) as fp:
